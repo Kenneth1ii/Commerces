95bd6f6f8ddc7a008846ebf8c3e96aa8ec132865
kennethli36 <likenneth36@gmail.com>
2021-04-12 22:45:45 -0400

    3rd

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import User # User from model.py class
 
 # Register your models here.
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
+    pass
+
+class AuctionListing(models.Model):
+    pass
+
+class AuctionBids(models.Model):
+    pass
+
+class AuctionComment(models.Model):
     pass
